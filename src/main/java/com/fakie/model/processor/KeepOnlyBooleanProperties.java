f1fbeed390e9fc62f0b67b798ad7529369099b37
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-08-15 16:41:40 +0200

    Fill boolean properties with false as a default value.

--- src/main/java/com/fakie/model/processor/KeepOnlyBooleanProperties.java
+++ src/main/java/com/fakie/model/processor/KeepOnlyBooleanProperties.java
@@ -1,9 +1,9 @@
 package com.fakie.model.processor;
 
+import com.fakie.model.graph.Element;
 import com.fakie.model.graph.Graph;
 import com.fakie.model.graph.Property;
 import com.fakie.model.graph.Type;
-import com.fakie.model.graph.Vertex;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,13 +13,24 @@ public class KeepOnlyBooleanProperties implements Processor {
     @Override
     public Graph process(Graph graph) throws ProcessingException {
         logger.info("Keep only boolean properties");
-        for (Vertex vertex : graph.getVertices()) {
-            for (Property property : vertex) {
+        for (Element element : graph.getElements()) {
+            for (Property property : element) {
                 if (property.getType() != Type.BOOLEAN) {
-                    vertex.removeProperty(property);
+                    element.removeProperty(property);
                 }
             }
         }
+        fillProperties(graph);
         return graph;
+    }
+
+    private void fillProperties(Graph graph) {
+        for (Element element : graph.getElements()) {
+            for (String key : graph.keys()) {
+                if (!element.hasProperty(key)) {
+                    element.setProperty(key, false);
+                }
+            }
+        }
     }
 }
